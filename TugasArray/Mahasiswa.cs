ee308f8d7e8bc5033774a5a7f940ef86bfac3bf4
Cornelia <cornelia@students.amikom.ac.id>
2023-07-15 16:02:16 +0700

    first commit

---
+++ TugasArray/Mahasiswa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasArray
+{
+    class Mahasiswa
+    {
+        public int Nomer { get; set; }
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public int Nilai { get; set; }
+
+    }
+}
---
+++ TugasArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mahasiswa1 = new Mahasiswa { Nomer = 1, Nama = "John Doe", Nilai = 44};
+            Mahasiswa mahasiswa2 = new Mahasiswa { Nomer = 2, Nama = "Jane Smith", Nilai = 66 };
+            Mahasiswa mahasiswa3 = new Mahasiswa { Nomer = 3, Nama = "Alice Johnson", Nilai = 77 };
+
+            Mahasiswa[] arrayMahasiswa = { mahasiswa1, mahasiswa2, mahasiswa3 };
+
+            foreach (Mahasiswa mahasiswa in arrayMahasiswa)
+            {
+                Console.WriteLine("Nomor: " + mahasiswa.Nomer);
+                Console.WriteLine("Nama: " + mahasiswa.Nama);
+                Console.WriteLine("Nilai: " + mahasiswa.Nilai);
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
